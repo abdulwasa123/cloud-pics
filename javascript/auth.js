3cffb6891e104e698a3ece414b22eaeae30614bc
abdulwasa123 <abdulwasayusuf@gmail.com>
2025-09-16 17:41:22 +0100

    .

--- javascript/auth.js
+++ javascript/auth.js
@@ -1,4 +1,4 @@
-        import { supabase } from "./supabase.js";
+        // import { supabase } from "./supabase.js";
         // Global variables
         let isDarkMode = false
         let currentForm = "login"
